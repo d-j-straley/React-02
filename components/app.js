a20bae40e5dd4b4d2245cb88221156a9d7ddd979
David J. Straley <djstraley@sandyhook.com>
2024-11-04 15:02:33 -0600

    removed this other project

--- components/app.js
+++ components/app.js
@@ -1,4 +1,5 @@
 import Banner from './banner';
+import HouseList from './houseList';
 
 const App = () => {
 
@@ -6,14 +7,15 @@ const App = () => {
 
     return (
 
-    <div>
+    <>
             {abc}
             <Banner>
                 <h2>
                     Every house comes with a helicopter pad!
                 </h2>
             </Banner>
-    </div>
+            <HouseList />
+    </>
     );
 };
 
